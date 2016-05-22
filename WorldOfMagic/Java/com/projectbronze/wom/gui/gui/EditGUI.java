306f2259154f545cd1c615573f0948ba542c4ae1
gt22 <igorengel@mail.ru>
2016-05-22 22:47:03 +0300

    Добавлена надпись в режим редактирования

--- WorldOfMagic/Java/com/projectbronze/wom/gui/gui/EditGUI.java
+++ WorldOfMagic/Java/com/projectbronze/wom/gui/gui/EditGUI.java
@@ -31,6 +31,7 @@ public class EditGUI extends GuiContainer {
     	int k = (width - xSize) / 2;
 		int l = (height - ySize) / 2;
 		drawTexturedModalRect(k, l, 0, 0, xSize, ySize);
+		drawString(fontRendererObj, "Edit Mode", k + 65, l + 15, 0x888888);
 	}
 	
 	@Override
