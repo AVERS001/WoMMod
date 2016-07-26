7d09568d626f9ec56aa23ee20f486e104b198e9e
gt22 <igorengel@mail.ru>
2016-07-26 21:53:26 +0300

    Исправлена гуишку блуд портала (Я там тестировал и случайно закомитил)

--- WorldOfMagic/Java/com/projectbronze/wom/gui/gui/BloodyCoreGUI.java
+++ WorldOfMagic/Java/com/projectbronze/wom/gui/gui/BloodyCoreGUI.java
@@ -1,15 +1,14 @@
 package com.projectbronze.wom.gui.gui;
 
+import org.lwjgl.opengl.GL11;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.util.ResourceLocation;
-import org.lwjgl.opengl.GL11;
-import com.gt22.gt22core.baseclasses.gui.GuiContainerGeneric;
 import com.projectbronze.wom.core.Core;
 import com.projectbronze.wom.gui.container.BloodyPortalContainer;
 import com.projectbronze.wom.tileEntity.BloodyCoreEntity;
 
-public class BloodyCoreGUI extends GuiContainerGeneric
+public class BloodyCoreGUI extends GuiContainer
 {
 	private BloodyCoreEntity te;
 	private static final ResourceLocation texture = new ResourceLocation(Core.modid, "textures/gui/BloodyPortal.png");
@@ -20,5 +19,15 @@ public class BloodyCoreGUI extends GuiContainerGeneric
 		this.xSize = 176;
 		this.ySize = 171;
 		this.te = te;
+	}
+
+	@Override
+	protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY)
+	{
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		mc.getTextureManager().bindTexture(texture);
+		int k = (width - xSize) / 2;
+		int l = (height - ySize) / 2;
+		drawTexturedModalRect(k, l, 0, 0, xSize, ySize);
 	}
 }
